42ff4d217c4956f86a0df92da7a805271262a068
Will <willmca32@gmail.com>
2019-07-29 22:22:29 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,19 +27,31 @@ function checkForMatch(){
   alert("You found a match!");
 } else {
   alert("Sorry, try again.");
-}
-}
 
-function flipCard(cardId){
+
+}
+}
+function flipCard(){
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
-	console.log("User flipped " + cards[cardId].suit);
-	console.log("User flipped " + cards[cardId].cardImage);
-
 	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+	this.setAttribute('src', cards[cardId].cardImage);
+
 	if (cardsInPlay.length === 2) 
 	{
 		checkForMatch();
 	}
 }
-flipCard(0);
-flipCard(2);
+function createBoard(){
+	for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard();
